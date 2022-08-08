491d456c2198db184d09675861469b7d514ce604
Champak's World <champaksworld@gmail.com>
2022-08-08 13:35:12 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -7,13 +7,18 @@ from .models import Person
 
 # Create your views here.
 def quiz(request):
+
     q1={"question":"What is C?","op1":"Language","op2":"Alphabet","op3":"Ascii character","op4":"All of these"}
     q2={"question":"who developed python programming language?","op1":"wick van rossum","op2":"rasmus harsh","op3":"guido van Rossum","op4":"none"}
     q3={"question":"which of the following is the correct extension of the python file?","op1":".python","op2":".pl","op3":".py","op4":".p"}
     q4={"question":"who developed c programming language ?","op1":"denies ritchies","op2":"shubham","op3":"harsh","op4":"none"}
     questions=[q1,q2,q3,q4]
+    questionno = 0
+    if request.POST:
+        questionno=request.POST["qno"]
+        questionno+=1
     #return httpResponse('python quiz!')
-    return render(request, "quiz.html",{"question":questions[0]})
+    return render(request, "quiz.html",{"question":questions[questionno]})
 
 def index(request):
     # return HttpResponse('Hello from Python!')
