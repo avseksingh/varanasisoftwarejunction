691ae05cde5b4a679f3943a6b8354dfe21256599
Champak's World <champaksworld@gmail.com>
2022-08-08 15:09:35 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -10,13 +10,13 @@ def quiz(request):
     q1 = {"question": "What is C?", "op1": "Language", "op2": "Alphabet", "op3": "Ascii character",
           "op4": "All of these","correct":"a"}
     q2 = {"question": "Who developed Python Programming language?", "op1": "Wick van rossum", "op2": "Dennis Ritches",
-          "op3": "Guido van Rossum", "op4": "none"}
+          "op3": "Guido van Rossum", "op4": "none","correct":"c"}
     q3 = {"question": "Which of the following is the correct extension of the python file?", "op1": ".python",
-          "op2": ".pl", "op3": ".py", "op4": ".p"}
+          "op2": ".pl", "op3": ".py", "op4": ".p","correct":"c"}
     q4 = {"question": "Who developed C programming language ?", "op1": "denies ritchies", "op2": "Guido van Rossum",
-          "op3": "harsh", "op4": "none"}
+          "op3": "harsh", "op4": "none","correct":"a"}
     q5 = {"question": "Django is  a ?", "op1": "Programming Language", "op2": "Framework",
-          "op3": "Python Web Framework", "op4": "None"}
+          "op3": "Python Web Framework", "op4": "None","correct":"c"}
     questions = [q1, q2, q3, q4, q5]
     questionno = 0
     if request.POST:
